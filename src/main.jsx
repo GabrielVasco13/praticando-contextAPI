5bc0dc0fcfbd6a0a9dd55b45ddce6e2b7ac6baa9
Gabriel FV <gabrielvasco906@gmail.com>
2025-01-06 16:00:52 -0300

    feat: criando o contextApi

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Rotas from "./routes";
 import "./reset.css";
+import { AppProvider } from "./contexts/AppContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Rotas />
+    <AppProvider>
+      <Rotas />
+    </AppProvider>
   </StrictMode>
 );
